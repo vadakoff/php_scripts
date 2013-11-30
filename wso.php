f3a7d3ad6b15cc17b816691d57ef955dcecda654
vadakoff <vadakoff@gmail.com>
2013-11-30 20:23:58 +0500

    Update wso.php

--- wso.php
+++ wso.php
@@ -3,7 +3,7 @@
 $color = "#df5";
 $default_action = 'FilesMan';
 $default_use_ajax = true;
-$default_charset = 'Windows-1251';
+$default_charset = 'UTF-8';
 
 if(!empty($_SERVER['HTTP_USER_AGENT'])) {
     $userAgents = array("Google", "Slurp", "MSNBot", "ia_archiver", "Yandex", "Rambler");
